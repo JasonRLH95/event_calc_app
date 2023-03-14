c04b039c4f3d67b51b8e95e2a28cd3d9e295d22c
JasonRLH95 <jason95.rm@gmail.com>
2023-03-14 18:41:50 +0200

    forth commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const numPeoples=(e)=>{let input1 = e.target.value;console.log(input1);return input1}
-const numtables=()=>{}
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+const numPeoples=()=>{
+    console.log(input1.value);return input1.value
+}
+const numtables=()=>{
+
+}
 const logResult=()=>{console.log(input1)}
