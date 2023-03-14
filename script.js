5eee7fc79c85261066091e9d2fd90b86c05e913f
JasonRLH95 <jason95.rm@gmail.com>
2023-03-14 18:28:40 +0200

    third commit

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-const numPeoples=(e)=>{let input1 = e.target.value;console.log(input1)}
+const numPeoples=(e)=>{let input1 = e.target.value;console.log(input1);return input1}
 const numtables=()=>{}
+const logResult=()=>{console.log(input1)}
